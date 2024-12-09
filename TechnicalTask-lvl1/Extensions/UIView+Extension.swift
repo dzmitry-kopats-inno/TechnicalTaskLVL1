250f96d4b240f1d7654b35b6f5e7076ffdbc5bcd
Dzmitry Kopats <dzmitry.kopats@innowise.com>
2024-12-09 19:32:59 +0100

    Reorder files

--- TechnicalTask-lvl1/Extensions/UIView+Extension.swift
+++ TechnicalTask-lvl1/Extensions/UIView+Extension.swift
@@ -1,5 +1,5 @@
 //
-//  UIView+Ex.swift
+//  UIView+Extension.swift
 //  TechnicalTask-lvl1
 //
 //  Created by Dzmitry Kopats on 25/11/2024.
@@ -8,9 +8,6 @@
 import UIKit
 
 extension UIView {
-    /**
-     Extends basic UIView functionality to add multiple subviews
-     */
     func addSubviews(_ views: [UIView]) {
         views.forEach { self.addSubview($0) }
     }
