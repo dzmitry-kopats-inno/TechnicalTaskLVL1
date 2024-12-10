0c53e6462380f43c3f15653286909baa8c752c88
Dzmitry Kopats <dzmitry.kopats@innowise.com>
2024-12-10 16:37:46 +0100

    Simplifying email verification

--- TechnicalTask-lvl1/AddUserModule/AddUserViewModel.swift
+++ TechnicalTask-lvl1/AddUserModule/AddUserViewModel.swift
@@ -65,7 +65,7 @@ final class AddUserViewModel: AddUserViewModelProtocol {
     }
 
     private func isValidEmail(_ email: String) -> Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailRegEx = ".+@.+"
         let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
         return emailPredicate.evaluate(with: email)
     }
